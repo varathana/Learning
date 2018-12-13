759fbe1e68baadf43d3826fc8c46b68d8afb2c4e
VA24086 <VA24086@HDQICTW513072.TLE.DPS>
2018-12-13 17:07:58 -0600

    added new java project to try java 8

---
+++ src/java8/ForEach.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package java8;
+
+/**
+ * @author VA24086
+ * 
+ * Use of forEach in Java 8 with before and after use cases
+ *
+ */
+public class ForEach {
+
+	/**
+	 * Test
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
